684ee22666eae036ed1555c8599235bafdc6de05
beye <beyemoktar@gmail.com>
2025-11-15 08:02:57 -0800

    Sprint 1 : Ajout du module Étudiants

---
+++ app/Http/Controllers/EtudiantController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Etudiant;
+use Illuminate\Http\Request;
+
+class EtudiantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $etudiants = \App\Models\Etudiant::all();
+        return view('etudiants', compact('etudiants'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Etudiant $etudiant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Etudiant $etudiant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Etudiant $etudiant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Etudiant $etudiant)
+    {
+        //
+    }
+}
+
